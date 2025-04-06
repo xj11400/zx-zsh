58966e2601837069db389504a9cd0f17d1a671a4
XJ Hsu <xj11400@gmail.com>
2025-04-06 20:16:45 +0800

    zpm: antidote

--- zpm/antidote/antidote.zsh
+++ zpm/antidote/antidote.zsh
@@ -1,5 +1,5 @@
 # Antidote's home folder
-export ANTIDOTE_HOME=$X_ZSH_PLUGIN_MANAGER_HOME/antidote
+export ANTIDOTE_HOME=${__zsh_plugin_manager_dir}/antidote
 
 # clone Antidote
 x_use "mattmc3/antidote" $ANTIDOTE_HOME/antidote
@@ -11,7 +11,17 @@ zstyle ':antidote:bundle' use-friendly-names on
 # Note: here using .zsh instead of .txt cause they are not in the same directory
 
 # Set the root name of the plugins files (.txt and .zsh) antidote will use.
-zsh_plugins=${ZDOTDIR:-$HOME/.config/zsh}/plugins/zpm/antidote/zsh_plugins.zsh
+zsh_plugins=(
+  ${ZDOTDIR:-$HOME/.config/zsh}/zpm/antidote/zsh_plugins.{zsh,txt}(N)
+  ${ZDOTDIR:-$HOME/.config/zsh}/zpm/zsh_plugins.{zsh,txt}(N)
+)
+(( $#zsh_plugins )) &&
+    zsh_plugins=$zsh_plugins[1] ||
+    {
+        zsh_plugins=$ZX_HOME/zpm/antidote/zsh_plugins.zsh
+        zstyle ':zx:load:config' plugin-manager yes
+    }
+
 
 # Ensure the .zsh_plugins.txt file exists so you can add plugins.
 [[ -f ${zsh_plugins} ]] || touch ${zsh_plugins}
